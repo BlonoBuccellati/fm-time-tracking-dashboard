df05b17aec48f85c8b2bfe88dfea151538b0af16
BlonoBuccellati <119178322+BlonoBuccellati@users.noreply.github.com>
2025-05-27 17:20:11 +0900

    fix

--- src/features/activity-tracker/services/getActivities.ts
+++ src/features/activity-tracker/services/getActivities.ts
@@ -1,13 +1,10 @@
 import { ActivityCard } from "@/types/activity-card";
 
-const ORIGIN =
-  process.env.NODE_ENV === "production"
-    ? `https://${process.env.VERCEL_URL}`
-    : "http://localhost:3000";
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL!;
 export async function getActivityCardsInfo(
   userId: string,
 ): Promise<ActivityCard[]> {
-  const res = await fetch(`${ORIGIN}/api/users/${userId}/activities`);
+  const res = await fetch(`${API_BASE}/api/users/${userId}/activities`);
   const userActivities: ActivityCard[] = await res.json();
   console.log(userActivities);
   return userActivities;
--- src/features/user/services/getUser.ts
+++ src/features/user/services/getUser.ts
@@ -3,12 +3,9 @@ import { User } from "@/features/user/types/user";
 // ❓ビジネスロジックは含んでいないが、servicesフォルダでいいのか？lib/apiの方がいいのか？
 // apiだとブラウザとのやり取りという認識になるか？
 // ただfetchしているだけだから、servicesは違和感を感じる？
-const ORIGIN =
-  process.env.NODE_ENV === "production"
-    ? `https://${process.env.VERCEL_URL}`
-    : "http://localhost:3000";
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL!;
 export async function getUser(userId: string): Promise<User> {
-  const userRes = await fetch(`${ORIGIN}/api/users/${userId}`);
+  const userRes = await fetch(`${API_BASE}/api/users/${userId}`);
   const data: User = await userRes.json();
   return data;
 }
